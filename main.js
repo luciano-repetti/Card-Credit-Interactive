6e1ba9714f796f3c1be5b3bb584b4fe56a2e0d5a
Luciano Repetti <lucianorepetti2020@gmail.com>
2022-11-01 18:07:58 -0300

    optimización de codigo(falta)

--- main.js
+++ main.js
@@ -1,42 +1,33 @@
 let inputName = document.getElementById("nameInput")
 let nameCard = document.getElementById("nameCard")
 
-inputName.addEventListener("keyup", (event) =>{
-    nameCard.innerHTML = event.target.value
-    if(nameInput.value.length === 0){
-        nameCard.innerHTML = "Menganito Perez"
-    }
-})
-
 let inputCvv = document.getElementById("cvvInput")
 let cvvCard = document.getElementById("cvvCard")
-
-inputCvv.addEventListener("keyup", (event) =>{
-    cvvCard.innerHTML = event.target.value
-    if(inputCvv.value.length === 0){
-        cvvCard.innerHTML = "000"
-    }
-})
 
 let mmCard = document.getElementById("mmCard")
 let inputMes = document.getElementById("mesInput")
 
-inputMes.addEventListener("keyup", (event) =>{
-    mmCard.innerHTML = event.target.value
-    if(inputMes.value.length === 0){
-        mmCard.innerHTML = "00"
-    }
-})
-
 let yyCard = document.getElementById("yyCard")
 let inputAnio = document.getElementById("anioInput")
 
-inputAnio.addEventListener("keyup", (event) =>{
-    yyCard.innerHTML = event.target.value
-    if(inputAnio.value.length === 0){
-        yyCard.innerHTML = "00"
-    }
-})
+function events(card, input, value){
+    var inputName = document.getElementById(input)
+    var nameCard = document.getElementById(card)
+    inputName.addEventListener("keyup", (event) => {
+        nameCard.innerHTML = event.target.value
+        if(inputName.value.length === 0){
+            nameCard.innerHTML = `${value}`
+        }
+    })
+}
+
+events("nameCard", "nameInput", "Menganito Perez")
+
+events(cvvCard, inputCvv, "000")
+
+events(mmCard, inputMes, "00")
+
+events(yyCard, inputAnio, "00")
 
 let numberCard = document.getElementById("numberCard")
 let inputNumber = document.getElementById("numberInput")
