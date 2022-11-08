7d7567d17d94a358d89ea256f078208f94a2a8f8
Luciano Repetti <lucianorepetti2020@gmail.com>
2022-11-08 18:08:00 -0300

    funciones validaciones

--- main.js
+++ main.js
@@ -4,33 +4,30 @@ let nameCard = document.getElementById("nameCard")
 let inputCvv = document.getElementById("cvvInput")
 let cvvCard = document.getElementById("cvvCard")
 
+let inputMonth = document.getElementById("mesInput")
 let mmCard = document.getElementById("mmCard")
-let inputMes = document.getElementById("mesInput")
 
+let inputYear = document.getElementById("anioInput")
 let yyCard = document.getElementById("yyCard")
-let inputAnio = document.getElementById("anioInput")
+
+let inputNumber = document.getElementById("numberInput")
+let numberCard = document.getElementById("numberCard")
+
+let btnsubmit = document.getElementById("submit")
 
 function events(card, input, value){
-    var inputName = document.getElementById(input)
-    var nameCard = document.getElementById(card)
-    inputName.addEventListener("keyup", (event) => {
-        nameCard.innerHTML = event.target.value
-        if(inputName.value.length === 0){
-            nameCard.innerHTML = `${value}`
+    input.addEventListener("keyup", (event) => {
+        card.innerHTML = event.target.value
+        if(input.value.length === 0){
+            card.innerHTML = `${value}`
         }
     })
 }
 
-events("nameCard", "nameInput", "Menganito Perez")
-
+events(nameCard, inputName, "Menganito Perez")
 events(cvvCard, inputCvv, "000")
-
-events(mmCard, inputMes, "00")
-
-events(yyCard, inputAnio, "00")
-
-let numberCard = document.getElementById("numberCard")
-let inputNumber = document.getElementById("numberInput")
+events(mmCard, inputMonth, "00")
+events(yyCard, inputYear, "00")
 
 inputNumber.addEventListener("keyup", (event) =>{
     numberCard.innerHTML = `<p>${event.target.value.slice(0,4)}</p><p>${event.target.value.slice(4,8)}</p><p>${event.target.value.slice(8,12)}</p><p>${event.target.value.slice(12,16)}</p>`
@@ -38,3 +35,28 @@ inputNumber.addEventListener("keyup", (event) =>{
         numberCard.innerHTML = `<p>0000</p><p>0000</p><p>0000</p><p>0000</p>`
     }
 })
+
+function verifName(nameFull) {
+    let validation = /^[a-zA-ZáéíóúÁÉÍÓÚ\s]{1,25}$/g;
+    return validation.test(nameFull);
+}
+
+function verifNumber(number) {
+    let validation = /^[0-9]{14,16}$/g;
+    return validation.test(number);
+}
+
+function verifMonth(month) {
+    let validation = /^(0[1-9]|1[0-2])$/g;
+    return validation.test(month);
+}
+
+function verifYear(year) {
+    let validation = /^(2[3-9]|3[0-9]|4[0-9])$/g;
+    return validation.test(year);
+}
+
+function verifCvv(cvv) {
+    let validation = /^[0-9]{3,4}$/g;
+    return validation.test(cvv);
+}
